64331b7ca9bffcac348acd1bdfca642e1ca36379
Monica Powell <mmp2181@barnard.edu>
2015-02-11 19:15:03 -0500

    added swig

--- app.js
+++ app.js
@@ -1,10 +1,31 @@
 //modules required for app 
 var express = require ( 'express' );
 var morgan = require( 'morgan' );
+var swig = require ('swig');
 
 
 //initialize app
 var app = express();
+
+//swig.renderFile renders html
+ app.engine('html', require('swig').renderFile);
+
+ //sets default view engine to html
+ app.set('view engine', 'html');
+ //this maps 
+
+//this path maps the views to our views path
+//__dirname is a variable is a node variable
+ //app.set('views', 'twitter-js' + '/views');
+app.set('views', __dirname + '/views');
+
+//turn of caching during development
+swig.setDefaults({ cache: false });
+
+//pass a local variable to the view 
+var people = [{name: 'Full'}, {name: 'Stacker'}, {name: 'Son'}];
+res.render('index', {title: 'Hall of Fame', people: people});
+
 
 //dev creates color coded status messages
 //https://github.com/expressjs/morgan#expressconnect
