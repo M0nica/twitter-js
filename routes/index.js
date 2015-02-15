f636f68921c20431b025b0fa43d41325223c07ab
Monica Powell <mmp2181@barnard.edu>
2015-02-14 19:05:26 -0500

    updated formatting

--- routes/index.js
+++ routes/index.js
@@ -11,3 +11,9 @@ router.get('/', function(req,res){
 });
 
 module.exports = router;
+
+router.get('/users/:name', function(req, res) {
+  var name = req.params.name;
+  var list = tweetBank.find( {name: name} );
+  res.render( 'index', { title: 'Twitter.js - Posts by '+name , list: list } );
+});
